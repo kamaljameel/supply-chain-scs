5c1fd56c2e15a00e803cfe2e5e0a104e960dc1fe
Shahab <shahabgul117@gmail.com>
2024-06-07 18:09:37 +0500

    C

---
+++ client/configs/mysql.js
@@ -0,0 +1,10 @@
+// config/database.js
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('supply-chain', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+module.exports = sequelize;
---
+++ client/utils/apiRoutes.js
@@ -0,0 +1,3 @@
+import { host } from "./apiHost";
+
+export const postcontactform = `${host}/contact`;
